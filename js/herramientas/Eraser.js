c63b0e1bd1abddd2c2e4bbfcaaa669b847e21166
Melanie San Roman <srmel123@gmail.com>
2025-04-24 14:29:17 -0300

    Se modifico la goma para un trazo mas parejo y prolijo

--- js/herramientas/Eraser.js
+++ js/herramientas/Eraser.js
@@ -1,15 +1,25 @@
 class Eraser extends Herramienta{
     constructor(posX, posY, context) {
         super(posX, posY, context);
+        this.context.lineCap = "round"; // suaviza la línea
     }
 
     /**
-     * Borra en el canvas utilizando el mouse
+     * Borra como una goma sobre el canvas siguiendo el mouse
      */
     execute(offsetX, offsetY) {
+
         this.context.beginPath();
-        this.context.arc(offsetX, offsetY, this.lineWidth, 0, 2 * Math.PI);
-        this.context.fill();
+        this.context.moveTo(this.posX, this.posY); // último punto
+        this.context.lineTo(offsetX, offsetY); // nuevo punto
+        this.context.lineWidth = this.lineWidth;
         this.context.stroke();
+
+        this.setXeY(offsetX, offsetY); // actualiza la posición actual
+    }
+
+    setXeY(posX, posY) {
+        this.posX = posX;
+        this.posY = posY;
     }
 }
